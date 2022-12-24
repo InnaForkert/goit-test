c0647027a249e92634c75fc2d888f20dc7ad1b65
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-24 03:03:45 +0200

    profile picture

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,10 @@
 import { Info } from "../Info/Info";
 import { ReactComponent as Logo } from "../../img/logo.svg";
 import { ReactComponent as HeaderPicture } from "../../img/header.svg";
-import { ReactComponent as ProfilePicture } from "../../img/photo.svg";
 import heart from "../../img/heart.png";
 import heartBot from "../../img/heartBot.png";
 import dots from "../../img/dots.png";
+import boy from "../../img/boy.png";
 
 import css from "./Card.module.css";
 
@@ -16,7 +16,8 @@ export function Card() {
       <img src={heart} alt="heart icon" className={css.heartIcon} />
       <img src={heartBot} alt="heart icon" className={css.heartBotIcon} />
       <img src={dots} alt="dots icon" className={css.dotsIcon} />
-      {/* <ProfilePicture /> */}
+      <div className={css.separator}></div>
+      <img src={boy} alt="a boy with red hair" className={css.profilePicture} />
       {/* <Info /> */}
     </li>
   );
