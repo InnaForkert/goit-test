53032b587372ac7f99af6b9607fc24c11adda5a1
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-24 21:01:03 +0200

    changed to useState

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,17 +1,24 @@
+import React, { useState } from "react";
 import css from "./Info.module.css";
+import { groupNumbers } from "../../utils/groupNumbers";
 
 export function Info({ tweets, followers }) {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <div className={css.infoSection}>
       <ul className={css.infoList}>
+        <li className={css.infoItem}>{groupNumbers(tweets)} TWEETS</li>
         <li className={css.infoItem}>
-          {tweets.toLocaleString("en", { useGrouping: true })} TWEETS
-        </li>
-        <li className={css.infoItem}>
-          {followers.toLocaleString("en", { useGrouping: true })} FOLLOWERS
+          {groupNumbers(followers + clicked)} FOLLOWERS
         </li>
       </ul>
-      <button className={css.button}>Follow</button>
+      <button
+        className={`${css.button} ${clicked ? css.clicked : null}`}
+        onClick={setClicked(!clicked)}
+      >
+        {clicked ? "Following" : "Follow"}
+      </button>
     </div>
   );
 }
